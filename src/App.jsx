82b5f7e5e67b7aafc0e030fe00079e61794f48cc
Sintey <sintay2729@gmail.com>
2025-08-20 16:38:58 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./Components/Hero/Hero";
 import About from "./Pages/AboutUs/AboutUs";
 import Navbar from "./Components/Navbar/Navbar";
 import Projects from "./Pages/Projects/Projects";
-import ContactUS from "./Pages/ContactUs/ContactUs";
+import ContactUs from "./Pages/ContactUs/ContactUs";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
           <Route path="/" element={<Hero />} />
           <Route path="/About" element={<About />} />
           <Route path="/Projects" element={<Projects />} />
-          <Route path="/Contact" element={<ContactUS />} />
+          <Route path="/Contact" element={<ContactUs />} />
         </Routes>
       </Router>
     </>
